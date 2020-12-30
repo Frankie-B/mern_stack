195e567b5d20a700692d2f2bb51f9ced6b167575
Frankie Bukenya <francis.bukenya@mediamonks.com>
2020-12-30 09:15:41 +0100

    Add intervals, timers, clearIntervals and timers, process.stdout

--- nodejs_essential_training/timers.js
+++ nodejs_essential_training/timers.js
@@ -3,7 +3,7 @@
 * they work they same way they do in the browser and are available
 * to us globally*/
 
-const waitTime = 3000;
+const waitTime = 5000;
 // incorporating setInterval
 const waitInterval = 500;
 let currentTime = 0;
@@ -11,30 +11,36 @@ let currentTime = 0;
 // increment time function
 const incTime = () => {
 	currentTime += waitInterval;
-	console.log(`Waiting ${currentTime / 1000} seconds`);
+	// calculate a percentage
+	// const percentage = Math.floor((currentTime / waitTime) * 100);
+	const percentage = (currentTime / waitTime) * 100 | 0; // floor the percentage
+	// console.log(percentage);
+	// console.log(`Waiting ${currentTime / 1000} seconds`);
+	process.stdout.clearLine(); // clears the last line written to the terminal
+	process.stdout.cursorTo(0); // move cursor to the beginning of the line
+	process.stdout.write(`Waiting...${percentage}%`);
 }
 
 // log wait time
-console.log(`setting a ${waitTime/1000} second delay`);
+console.log(`setting a ${waitTime / 1000} second delay`);
 
 // function to let us now the timer is finished
 const timerFinished = () => {
-	console.log(`timer finished!`);
+	clearInterval(interval);
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	console.log(`Timer is finished!`);
 };
 
 /* setInterval - the first arg it takes is a function, the second arg
 * is the duration of the interval*/
 
 // create a variable for the interval
-const interval = setInterval(incTime,waitInterval);
+const interval = setInterval(incTime, waitInterval);
 /* The setInterval function returns the interval to allow us to be able
 * to clear the interval later on*/
-
-
 
 /* when using the setTimeout function the first arg is the function
 * that should run when the timer is done, the second arg is the time
 * that we should wait*/
 setTimeout(timerFinished, waitTime);
-
-/**/
