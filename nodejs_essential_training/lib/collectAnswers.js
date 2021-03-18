3e96cb2ef12bd40c3a61ba66dcb0efd26e22160b
Frankie Bukenya <frankie.bukenya@gmail.com>
2021-03-18 16:43:21 +0100

    +(ex): Add improvement to module with event emitter

--- nodejs_essential_training/lib/collectAnswers.js
+++ nodejs_essential_training/lib/collectAnswers.js
@@ -1,4 +1,6 @@
 const readline = require('readline');
+const {EventEmitter} = require('events');
+
 // create readline interface for question to be asked in the terminal
 const rl = readline.createInterface({
 	input: process.stdin,
@@ -10,15 +12,22 @@ module.exports = (questions, done = f => f) => {
 	const answers = [];
 	// Destructure the questions
 	const [firstQuestion] = questions;
+	// create a new instance of the event emitter
+	const emitter = new EventEmitter();
 
 	const questionAnswered = answer => {
+		emitter.emit('answer', answer);
 		answers.push(answer);
 		if (answers.length < questions.length) { // check if the question was answered and ask next question
 			rl.question(questions[answers.length], questionAnswered);
 		} else {
+			emitter.emit('complete', answers);
 			done(answers);
 		}
 	}
 
 	rl.question(firstQuestion, questionAnswered);
+
+
+	return emitter;
 }
--- nodejs_essential_training/questions.js
+++ nodejs_essential_training/questions.js
@@ -6,9 +6,15 @@ const questions = [
 	'what are you going to do with nodejs?',
 ];
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions)
+
+answerEvents.on("answer", (answer) => {
+	console.log(`question answered: ${answer}`);
+})
+
+answerEvents.on("complete", answers => {
 	console.log('Thank you for your answers.');
 	console.log(answers);
-
-	process.exit();
 })
+
+answerEvents.on("complete", () => process.exit());
